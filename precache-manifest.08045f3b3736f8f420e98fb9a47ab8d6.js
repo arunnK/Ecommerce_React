b1a4c0912c64ee56725b94d592a4af5b881a48d5
Arun Kumar <arunkumar7617@gmail.com>
2019-07-11 00:36:20 +0400

    Updates

---
+++ precache-manifest.08045f3b3736f8f420e98fb9a47ab8d6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25d8306264c6da7a9f0873d253044faf",
+    "url": "/Monsters-Rolodex/index.html"
+  },
+  {
+    "revision": "802ff79b7144f0eb2b2f",
+    "url": "/Monsters-Rolodex/static/css/main.e8eb00e9.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/Monsters-Rolodex/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "802ff79b7144f0eb2b2f",
+    "url": "/Monsters-Rolodex/static/js/main.40d3b9ac.chunk.js"
+  },
+  {
+    "revision": "3c42c109e430c626051d",
+    "url": "/Monsters-Rolodex/static/js/runtime~main.2f1dcf62.js"
+  }
+]);
